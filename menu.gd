acde5c501acf1bc83b4c697b85422885a8638d35
jemronolo <jaronolo@up.edu.ph>
2024-12-11 01:49:27 +0800

    initial commit

--- menu.gd
+++ menu.gd
@@ -4,7 +4,10 @@ extends Control
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_play_button_pressed():
 	print("Join Game")
 	get_tree().change_scene_to_file("res://joingame.tscn")
+
+func _on_help_button_pressed():
+	print("Help")
+	get_tree().change_scene_to_file("res://helpmenu.tscn")
