8e8815bb8d34dfac1a8a364a39c2547b863a39cf
tnrbns <tsrubinos@up.edu.ph>
2024-12-10 21:40:47 +0800

    emission

--- src/scripts/testscene.gd
+++ src/scripts/testscene.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
 var block = preload("res://elements/block2.tscn")
+var timelabel: Label
+var timer: Timer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var start_x = 900
 	var start_y = 110
 	var column_gap = 70  # Horizontal gap between columns
-	var row_gap = 70    # Vertical gap between rows
+	var row_gap = 70     # Vertical gap between rows
 
 	for col in range(8): 
 		for row in range(11): 
@@ -15,7 +17,12 @@ func _ready():
 			var y_position = start_y + row * row_gap
 			inst(Vector2(x_position, y_position))
 			
-func inst(pos):
+	timelabel = $TimeLabel
+	timer = $Timer
+	
+	timer.start()
+			
+func inst(pos: Vector2):
 	var instance = block.instantiate()
 	add_child(instance)
 	instance.call_deferred("set_position", pos)
@@ -23,9 +30,13 @@ func inst(pos):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$ScoreP1.text = str(GameManager.score)
+	timelabel.text = time_to_minutes_secs_mili(timer.get_time_left())
 	
-func getColors():
-	var colors = [
-		
-	]
-	return colors
+func time_to_minutes_secs_mili(time: float) -> String:
+	var mins = int(time) / 60
+	var secs = int(time) % 60
+	var mili = int((time - int(time)) * 100)
+	return str("%0*d" % [2, mins]) + ":" + str("%0*d" % [2, secs]) 
+
+func _on_timer_timeout():
+	print("DONE!")
