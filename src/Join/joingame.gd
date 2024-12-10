0f25fe7ca2a6b3fe74e139f33fb5cc21afeaed4c
tnrbns <tsrubinos@up.edu.ph>
2024-12-11 06:10:29 +0800

    change textbox behavior join game

--- src/Join/joingame.gd
+++ src/Join/joingame.gd
@@ -1,7 +1,7 @@
 extends Control
 
 # Use `@onready` without the @ symbol
-@onready var join_button = $VBoxContainer4/JoinButton
+@onready var join_button = $JoinButton
 @onready var enter_ip_address = $VBoxContainer/EnterIPAddress
 @onready var enter_player_name = $VBoxContainer/EnterPlayerName
 
@@ -10,8 +10,8 @@ func _ready():
 	# Disable the join button by default
 	join_button.disabled = true
 	# Connect the text_submitted signals to the appropriate functions
-	enter_ip_address.text_submitted.connect(_on_enter_ip_address_text_submitted)
-	enter_player_name.text_submitted.connect(_on_enter_player_name_text_submitted)
+	enter_ip_address.text_changed.connect(_on_enter_ip_address_text_changed)
+	enter_player_name.text_changed.connect(_on_enter_player_name_text_changed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta):
@@ -26,19 +26,7 @@ func _on_back_button_pressed():
 func _on_join_button_pressed():
 	print("Start Gameplay")
 	# Add your gameplay logic here
-
-# Called when the IP address is submitted
-func _on_enter_ip_address_text_submitted(IP_text: String) -> void:
-	print("IP Address: ", IP_text)
-	# Enable the button if both fields are filled
-	_check_button_enabled()
-
-# Called when the player name is submitted
-func _on_enter_player_name_text_submitted(PlayerName_text: String) -> void:
-	print("Player Name: ", PlayerName_text)
-	# Enable the button if both fields are filled
-	_check_button_enabled()
-
+	
 # Function to check if both text fields are filled and enable the button
 func _check_button_enabled():
 	# Enable the button if both the IP address and player name are provided
@@ -46,3 +34,13 @@ func _check_button_enabled():
 		join_button.disabled = false
 	else:
 		join_button.disabled = true
+
+func _on_enter_player_name_text_changed(PlayerName_text: String):
+	print("Player Name: ", PlayerName_text)
+	# Enable the button if both fields are filled
+	_check_button_enabled()
+
+func _on_enter_ip_address_text_changed(IP_text: String):
+	print("IP Address: ", IP_text)
+	# Enable the button if both fields are filled
+	_check_button_enabled()
