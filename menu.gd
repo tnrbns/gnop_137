a3a57e5b974779cca4d335380bb80b8d9d0eebfd
NheilChristianDelaCruz <nheil.delacruz123@gmail.com>
2024-12-10 16:28:05 +0800

    add host game, edit texts, no connection muna

--- menu.gd
+++ menu.gd
@@ -8,3 +8,8 @@ func _on_quit_button_pressed() -> void:
 func _on_play_button_pressed():
 	print("Join Game")
 	get_tree().change_scene_to_file("res://joingame.tscn")
+
+
+func _on_host_button_pressed():
+	print("Host Game")
+	get_tree().change_scene_to_file("res://hostgame.tscn")# Replace with function body.
