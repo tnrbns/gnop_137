3e2cb50c56f561afbbe4e48e32db838dc494fc32
NheilChristianDelaCruz <nheil.delacruz123@gmail.com>
2024-12-10 13:18:07 +0800

    connecting join game from main menu

--- menu.gd
+++ menu.gd
@@ -3,3 +3,8 @@ extends Control
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_play_button_pressed():
+	print("Join Game")
+	get_tree().change_scene_to_file("res://joingame.tscn")
